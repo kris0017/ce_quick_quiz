33a233f2966f9b4d47c253925ae115f3ba5f08b1
Khrystyna Mosolova <kristina.mosolova@gmail.com>
2016-11-25 23:29:25 +0200

    Added tests for registration ctrl.

--- test/spec/controllers/registration.js
+++ test/spec/controllers/registration.js
@@ -5,19 +5,72 @@ describe('Controller: RegistrationCtrl', function () {
   // load the controller's module
   beforeEach(module('ceQuickQuizApp'));
 
-  var RegistrationCtrl,
-    scope;
+  var RegistrationCtrl, scope, location, log, deferred, userFactory, fakeUser;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    RegistrationCtrl = $controller('RegistrationCtrl', {
-      $scope: scope
-      // place here mocked dependencies
+  beforeEach(inject(function (_$controller_, _$location_, _$rootScope_, _userFactory_, _$q_, _$log_) {
+    scope = _$rootScope_.$new();
+    deferred = _$q_.defer();
+    location = _$location_;
+    log = _$log_;
+    userFactory = _userFactory_;
+
+    spyOn(userFactory, 'add').and.callFake(function () {
+      return deferred.promise;
     });
+    
+    spyOn(location, 'path').and.callThrough();
+
+    RegistrationCtrl = _$controller_('RegistrationCtrl', {
+      $scope: scope,
+      $location: location,
+      $log: log,
+      userFactory: userFactory
+    });  
+
+    createVariables();   
   }));
 
-  /*it('should attach a list of awesomeThings to the scope', function () {
-    //expect(ResultsCtrl.awesomeThings.length).toBe(3);
-  });*/
+  function createVariables() {
+    fakeUser = {
+      email: 'email@gmail.com',
+      password: 'fakepass'
+    };
+  }
+
+  it('ctrl should be defined and variables should be initialized', function() {
+    expect(RegistrationCtrl).toBeDefined();
+    expect(scope.labels).toEqual({ btn: 'Registration'});
+    expect(scope.form).toEqual({ invalidEmail: false, invalidPassword: false });
+    expect(scope.user).toEqual({ });
+  });
+
+  it('test for enterQuiz when user created', function() {    
+    scope.user = fakeUser;
+    
+    scope.enterQuiz();
+
+    expect(userFactory.add).toHaveBeenCalled();
+    
+    deferred.resolve(); 
+    
+    scope.$digest(); 
+    expect(location.path).toHaveBeenCalled();
+    expect(location.path).toHaveBeenCalledWith('/quiz');
+    expect(log.info.logs).toContain(['User was successfully created']);
+  });
+
+  it('test for enterQuiz when error occurred while saving new user', function() {    
+    scope.user = fakeUser;
+    
+    scope.enterQuiz();
+
+    expect(userFactory.add).toHaveBeenCalled();
+    
+    deferred.reject(); 
+    
+    scope.$digest(); 
+    expect(log.error.logs).toContain(['User was not created! An error occurred!']);
+  });
+
 });
